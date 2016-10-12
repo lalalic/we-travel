0cfdc4f076e7a71c061a554cf5945f8c4f486e0a
licheng <lalalic@139.com>
2016-10-13 06:31:22 +0800

    photoos self service

--- plugins/photoPos/src/android/com/lalalic/wetravel/PhotoPosPlugin.java
+++ plugins/photoPos/src/android/com/lalalic/wetravel/PhotoPosPlugin.java
@@ -12,6 +12,8 @@ import android.content.Context;
 import android.content.Intent;
 
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.media.ExifInterface;
 import android.provider.MediaStore;
 import android.text.format.DateUtils;
@@ -21,74 +23,90 @@ import java.util.Date;
 
 public class PhotoPosPlugin extends CordovaPlugin{
 	public final static String TAG="we.travel";
+    public static TravelDB dbHelper=null;
 
 	protected void pluginInitialize(){
 		this.cordova.getActivity().sendBroadcast(new Intent("com.lalalic.wetravel.PhotoPos"));
+        dbHelper=new TravelDB(this.cordova.getActivity().getApplicationContext());
 		Log.i(TAG,"photoPos plugin initialized");
 	}
 
 	@Override
 	public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-		if("extract".equals(action)){
-			final Context context=this.cordova.getActivity().getApplicationContext();
-			final long from=args.getLong(0);
-			final long to= args.getLong(1);
+		if ("extract".equals(action)) {
+			final Context context = this.cordova.getActivity().getApplicationContext();
+			final long from = args.getLong(0);
+			final long to = args.getLong(1);
 			cordova.getThreadPool().execute(new Runnable() {
 				@Override
 				public void run() {
-					int counter=0;
-					Cursor cursor=context.getContentResolver().query(
+					int counter = 0;
+					Cursor cursor = context.getContentResolver().query(
 							MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-							,new String[]{
+							, new String[]{
 									MediaStore.MediaColumns.DATA
-									,MediaStore.Images.ImageColumns.DATE_TAKEN
-									,MediaStore.Images.ImageColumns.LATITUDE
-									,MediaStore.Images.ImageColumns.LONGITUDE
+									, MediaStore.Images.ImageColumns.DATE_TAKEN
+									, MediaStore.Images.ImageColumns.LATITUDE
+									, MediaStore.Images.ImageColumns.LONGITUDE
 							}
-							,"datetaken>=? AND datetaken<=? AND mime_type=? and _data like ?"
-							,new String[]{String.valueOf(from),String.valueOf(to),"image/jpeg", "%DCIM/Camera%"}
-							,null);
-					try{
-						while(cursor.moveToNext()){
-							int i=0;
+							, "datetaken>=? AND datetaken<=? AND mime_type=? and _data like ?"
+							, new String[]{String.valueOf(from), String.valueOf(to), "image/jpeg", "%DCIM/Camera%"}
+							, null);
+                    TravelDB dbHelper=new TravelDB(context);
+                    SQLiteDatabase db=dbHelper.getWritableDatabase();
+                    try {
+                        while (cursor.moveToNext()) {
+							int i = 0;
 							String filePath = cursor.getString(i++);
-							long taken=cursor.getLong(i++);
-							float lat=cursor.getFloat(i++);
-							float lng=cursor.getFloat(i++);
-							float[] loc=null;
-							if(lat!=0 || lng!=0)
-								loc=new float[]{lat,lng};
+							long taken = cursor.getLong(i++);
+							float lat = cursor.getFloat(i++);
+							float lng = cursor.getFloat(i++);
+							float[] loc = null;
+							if (lat != 0 || lng != 0)
+								loc = new float[]{lat, lng};
 							else
-								loc=PhotoPosPlugin.readPosInExif(filePath);
+								loc = PhotoPosPlugin.readPosInExif(filePath);
 
-							if(loc!=null) {
-								Log.i(TAG+".GPS", "a photo taken at " + loc[0]+","+loc[1] + " on " + taken);
+							if (loc != null) {
+								Log.i(TAG + ".GPS", "a photo taken at " + loc[0] + "," + loc[1] + " on " + taken);
 								counter++;
+                                db.execSQL("insert or abort into photopos(taken,lat,lng,path,uploaded) values (?,?,?,?,?)"
+                                        , new Object[]{taken, loc[0], loc[1], filePath, 0});
+
+                                /*
 								PluginResult r=new PluginResult(PluginResult.Status.OK,new JSONObject()
-									.put("_id",taken)
-									.put("when",taken)
-									.put("loc",new JSONObject()
-											.put("type","Point")
-											.put("coordinates",new JSONArray().put(loc[0]).put(loc[1])))
-									.put("photo",filePath));
+										.put("_id",taken)
+										.put("when",taken)
+										.put("loc",new JSONObject()
+												.put("type","Point")
+												.put("coordinates",new JSONArray().put(loc[0]).put(loc[1])))
+										.put("photo",filePath));
 								r.setKeepCallback(true);
 								callbackContext.sendPluginResult(r);
+								*/
 							}
 						}
-						Log.d(TAG+".GPS", "found "+counter+" photos with position information");
-						callbackContext.success(counter);
-					}catch(Exception ex){
-						Log.e(TAG,ex.getMessage(), ex);
+                        context.getSharedPreferences(TAG,Context.MODE_PRIVATE)
+                                .edit()
+                                .putLong("lastExtractPhotoPosTime",new Date().getTime())
+                                .commit();
+						Log.d(TAG + ".GPS", "found " + counter + " photos with position information");
+						callbackContext.success(0);
+					} catch (Exception ex) {
+						Log.e(TAG, ex.getMessage(), ex);
 						callbackContext.error(ex.getMessage());
-					}finally{
+					} finally {
 						cursor.close();
+                        dbHelper.close();
 					}
 
 
 				}
 			});
 			return true;
-		}
+		}else if("query".equals(action)){
+            new
+        }
 		return false;
 	}
 
@@ -105,7 +123,42 @@ public class PhotoPosPlugin extends CordovaPlugin{
 		return null;
 	}
 
-	public static int save(Context ctx, float[] loc, long takentime, String filePath){
-		return 0;
+	public static void save(Context ctx, float[] loc, long takentime, String filePath){
+        SQLiteOpenHelper helper=new TravelDB(ctx);
+        try {
+            helper.getWritableDatabase()
+                    .execSQL("insert into photopos(taken,lat,lng,path,uploaded) values (?,?,?,?,?)"
+                            , new Object[]{takentime, loc[0], loc[1], filePath, 0});
+
+        }catch(Exception ex){
+            Log.e(TAG, ex.getMessage(), ex);
+        }finally{
+            helper.close();
+        }
 	}
+
+    private static class TravelDB extends SQLiteOpenHelper {
+
+        TravelDB(Context ctx){
+            super(ctx,TAG+".db", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            Log.d(TAG,"creating we-travel database");
+
+            db.execSQL("CREATE TABLE photopos ("
+                    + "taken INTEGER PRIMARY KEY"
+                    + ",lat real"
+                    + ",lng real"
+                    + ",path text"
+                    + ",uploaded integer default '0'"
+                    + ");");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+    }
 }
